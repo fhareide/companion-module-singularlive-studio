1c37cd26bcd4336bea09bf8815a6cfa61d2b7e0d
fhareide <fredrik.hareide@gmail.com>
2021-10-15 13:56:04 +0200

    Added feedbacks and presets. Changed all api calls to use id's instead of names. Merged actions and actionsUI.

--- index.js
+++ index.js
@@ -3,103 +3,61 @@ const instance_skel = require('../../instance_skel')
 const api = require('./lib/api')
 
 const actions = require('./lib/actions')
-const actionUI = require('./lib/actionsUI')
+const feedbacks = require('./lib/feedbacks')
+const presets = require('./lib/presets')
 
 class instance extends instance_skel {
 	constructor(system, id, config) {
 		super(system, id, config)
+
 		Object.assign(this, {
-			...actionUI,
 			...actions,
+			...feedbacks,
+			...presets,
 		})
 
-		return this
+		this.compositions = []
+		this.controlnodes = []
+		this.buttons = []
+		this.checkboxes = []
+		this.timers = []
+		this.states = {}
+	}
+
+	actions(system) {
+		this.setActions(this.getActions())
+	}
+
+	action({ action, options }) {
+		this[action](options)
+			.then(() => {
+				this.status(this.STATUS_OK, 'Ready')
+			})
+			.catch((e) => {
+				this.handleError(e)
+			})
+	}
+
+	feedbacks(system) {
+		this.setFeedbackDefinitions(this.getFeedbacks())
+	}
+
+	presets(system) {
+		this.setPresetDefinitions(this.getPresets())
 	}
 
 	init() {
 		this.initSingularLive(this.config)
 	}
 
+	destroy() {
+		this.debug('destroy', this.id)
+		this.stopUpdatePoller()
+	}
+
 	updateConfig(config) {
 		this.config = config
 		this.initSingularLive(this.config)
-	}
-
-	async initSingularLive(config) {
-		try {
-			this.SingularLive = new api(config.apiurl)
-
-			await this.SingularLive.Connect().catch((err) => {
-				if (err.toString().toLowerCase() == 'not found') {
-					const str = config.apiurl ? 'Invalid token' : 'Please enter a token'
-					this.log('warn', str)
-					throw new Error(str)
-				} else {
-					this.log('warn', err)
-					throw new Error(err)
-				}
-			})
-
-			const compositions = []
-			const controlnodes = []
-			const buttons = []
-			const checkboxes = []
-			const timers = []
-
-			await this.SingularLive.getElements()
-				.then((res) => {
-					for (let i = 0; i < res.length; i++) {
-						if (res[i].name) {
-							compositions.push({
-								id: res[i].name,
-								label: res[i].name,
-							})
-
-							if (res[i].nodes) {
-								let nodes = res[i].nodes.reduce((r, c) => Object.assign(r, c), {})
-
-								const keys = Object.keys(nodes)
-								for (let j = 0; j < keys.length; j++) {
-									const node = nodes[keys[j]]
-
-									const controlNode = {
-										id: res[i].name + '&!&!&' + node.id,
-										label: res[i].name + ' / ' + keys[j],
-									}
-
-									if (node.type == 'text' || node.type == 'number' || node.type == 'textarea' || node.type == 'image') {
-										controlnodes.push(controlNode)
-									} else if (node.type == 'button') {
-										buttons.push(controlNode)
-									} else if (node.type == 'timecontrol') {
-										timers.push(controlNode)
-									} else if (node.type == 'checkbox') {
-										checkboxes.push(controlNode)
-									}
-								}
-							}
-						}
-					}
-				})
-				.catch((err) => {
-					this.log('warn', err)
-					throw new Error(err)
-				})
-			this.system.emit(
-				'instance_actions',
-				this.id,
-				this.getActions(compositions, controlnodes, buttons, checkboxes, timers)
-			)
-
-			this.status(this.STATUS_OK, 'OK')
-		} catch (e) {
-			this.debug(e.message)
-			this.status(this.STATUS_WARNING, e.message)
-		}
-	}
-
-	destroy() {
-		this.debug('destroy', this.id)
 	}
 
 	config_fields() {
@@ -122,6 +80,124 @@ class instance extends instance_skel {
 		]
 	}
 
+	async initSingularLive(config) {
+		try {
+			this.SingularLive = new api(config.apiurl)
+
+			await this.SingularLive.connect().catch((err) => {
+				if (err.toString().toLowerCase() == 'not found') {
+					const str = config.apiurl ? 'Invalid token' : 'Please enter a token'
+					this.log('warn', str)
+					throw new Error(str)
+				} else {
+					this.log('warn', err)
+					throw new Error(err)
+				}
+			})
+
+			this.updateCompositions()
+
+			this.startUpdatePoller()
+
+			this.status(this.STATUS_OK, 'OK')
+		} catch (e) {
+			this.debug(e.message)
+			this.status(this.STATUS_WARNING, e.message)
+		}
+	}
+
+	async updateCompositions() {
+		this.compositions = []
+		this.controlnodes = []
+		this.buttons = []
+		this.checkboxes = []
+		this.timers = []
+
+		await this.SingularLive.getElements()
+			.then((res) => {
+				for (let i = 0; i < res.length; i++) {
+					if (res[i].name) {
+						this.states[res[i].id] = res[i].state
+						this.compositions.push({
+							id: res[i].id,
+							label: res[i].name,
+						})
+
+						if (res[i].nodes) {
+							let nodes = res[i].nodes.reduce((r, c) => Object.assign(r, c), {})
+
+							const keys = Object.keys(nodes)
+							for (let j = 0; j < keys.length; j++) {
+								const node = nodes[keys[j]]
+
+								const controlNode = {
+									type: 'textinput',
+									id: res[i].id + '&!&!&' + node.id,
+									label: res[i].name + ' / ' + keys[j],
+								}
+
+								if (
+									node.type == 'text' ||
+									node.type == 'number' ||
+									node.type == 'textarea' ||
+									node.type == 'image' ||
+									node.type == 'json'
+								) {
+									this.controlnodes.push(controlNode)
+								} else if (node.type == 'button') {
+									this.buttons.push(controlNode)
+								} else if (node.type == 'timecontrol') {
+									this.timers.push(controlNode)
+								} else if (node.type == 'checkbox') {
+									this.checkboxes.push(controlNode)
+								}
+							}
+						}
+					}
+				}
+			})
+			.catch((err) => {
+				this.log('warn', err)
+				throw new Error(err)
+			})
+
+		this.actions()
+		this.presets()
+		this.feedbacks()
+		this.checkFeedbacks()
+	}
+
+	async updateStates() {
+		await this.SingularLive.getStates()
+			.then((res) => {
+				for (let i = 0; i < res.length; i++) {
+					this.states[res[i].id] = res[i].state
+				}
+				this.checkFeedbacks()
+			})
+			.catch((err) => {
+				this.log('warn', err)
+				throw new Error(err)
+			})
+	}
+
+	startUpdatePoller() {
+		this.stopUpdatePoller()
+
+		this.updatePoller = setInterval(() => {
+			if (this.SingularLive) {
+				this.updateStates()
+			}
+		}, 1000)
+	}
+
+	stopUpdatePoller() {
+		if (this.updatePoller) {
+			clearInterval(this.updatePoller)
+			this.updatePoller = null
+		}
+	}
+
 	handleConnectionError() {
 		this.log('error', 'Singular.Live connection lost')
 		this.status(this.STATUS_ERROR, 'Connection error')
@@ -134,16 +210,6 @@ class instance extends instance_skel {
 			this.log('error', error.message)
 			this.debug(error)
 		}
-	}
-
-	action({ action, options }) {
-		this[action](options)
-			.then(() => {
-				this.status(this.STATUS_OK, 'Ready')
-			})
-			.catch((e) => {
-				this.handleError(e)
-			})
 	}
 }
 
